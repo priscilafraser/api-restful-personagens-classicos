1f228ae486c0831f693c1995d4203af30df236e7
priscilafraser <priscila.fraser@gmail.com>
2021-09-16 20:03:26 -0400

    ajustes para o herokU

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 
+const dbAtlas = process.env.DATABASE_ATLAS;
 const connectToDb = () => {
   mongoose.connect(process.env.DATABASE_ATLAS, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  });
+  const uri = "mongodb+srv://fraser:4qx3xoO6Dko3NFNs@cluster0.yvygw.mongodb.net/db_personagensclassicos";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(err => {
+    const collection = client.db("db_personagensclassicos").collection("personagems");
+    // perform actions on the collection object
+    client.close();
   });
 };
 
