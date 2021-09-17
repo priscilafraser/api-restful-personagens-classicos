06b76d5bd4dfd2228bf3943d2c8ed32f54e2deca
priscilafraser <priscila.fraser@gmail.com>
2021-09-16 20:15:58 -0400

    ajustes para o herokU

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
-const { MongoClient } = require('mongodb');
 
 const dbAtlas = process.env.DATABASE_ATLAS;
 const connectToDb = () => {
-  
-  const uri = dbAtlas;
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  client.connect(err => {
-    const collection = client.db("db_personagensclassicos").collection("personagems");
-    // perform actions on the collection object
-    client.close();
+  mongoose.connect(process.env.DATABASE_ATLAS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 };
 
